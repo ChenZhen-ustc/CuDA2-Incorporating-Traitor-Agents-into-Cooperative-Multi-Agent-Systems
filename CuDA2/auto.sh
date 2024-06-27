4c432c6d7d1498806a7c710f596443e2fb56ae54
ChenZhen <37293452+ChenZhen-ustc@users.noreply.github.com>
2024-06-27 11:02:13 +0800

    Add files via upload

---
+++ CuDA2/auto.sh
@@ -0,0 +1,5 @@
+python3 src/main.py --config=qmix action_type=origin traitor_num=0 --env-config=sc2 with env_args.map_name=6m_vs_6m
+python3 src/main.py --config=qmix action_type=stop traitor_num=1 --env-config=sc2 with env_args.map_name=7m_vs_6m
+python3 src/main.py --config=qmix action_type=random traitor_num=1 --env-config=sc2 with env_args.map_name=7m_vs_6m
+python3 src/main.py --config=qmix action_type=minus_r traitor_num=1 --env-config=sc2 with env_args.map_name=7m_vs_6m
+python3 src/main.py --config=qmix action_type=PBRS_RND traitor_num=1 --env-config=sc2 with env_args.map_name=7m_vs_6m
